e729e323a52b44e26b2fa3092ecca0b93fab3fcd
Cristian Kubrak <kubrakcristian@gmail.com>
2017-11-10 10:14:49 -0300

    Test de suma de matrices

--- tests/test-matrix-mult.cpp
+++ tests/test-matrix-mult.cpp
@@ -30,19 +30,43 @@ TEST(test_multiplicacion, test_matriz_cuadrada) {
                   ));
 }
 
-TEST(test_multiplicacion, test_matriz_gigante) {
-    // una matriz un poco más grande
-    Matriz M4 = crear(1024,1.0);
-    auto t0 = std::chrono::system_clock::now();
-    Matriz M5 = multiplicar_strassen(M4, M4, 32);
-    auto t1 = std::chrono::system_clock::now();
-    EXPECT_FLOAT_EQ(M5[0][0], M5.size());
-
-    auto t2 = std::chrono::system_clock::now();
-    Matriz M6 = multiplicar_strassen(M4, M4, M4.size());
-    auto t3 = std::chrono::system_clock::now();
-    EXPECT_FLOAT_EQ(M5[0][0], M5.size());
-    auto d = double((t1-t0).count()) / (t3-t2).count();
-    std::cout << "D: " << d << std::endl;
-    EXPECT_LT(d, 0.4);
+TEST(test_suma, suma_ceros){
+  Matriz A = crear(4,0);
+  Matriz B = crear(4,0);
+  Matriz C = sumar(A,B);
+  EXPECT_EQ(A,C);
 }
+
+TEST(test_suma, suma_con_cero){
+  Matriz A = crear(8,1.2);
+  Matriz B = crear(8,0);
+  Matriz C = sumar(A,B);
+  EXPECT_EQ(A,C);
+}
+
+TEST(test_suma, suma_normal){
+  Matriz A = crear(3,1.2);
+  Matriz B = crear(3,0.1);
+  Matriz C = {
+    {1.3,1.3,1.3},
+    {1.3,1.3,1.3},
+    {1.3,1.3,1.3}
+  };
+  EXPECT_EQ(sumar(A,B),C);
+}
+// TEST(test_multiplicacion, test_matriz_gigante) {
+//     // una matriz un poco más grande
+//     Matriz M4 = crear(1024,1.0);
+//     auto t0 = std::chrono::system_clock::now();
+//     Matriz M5 = multiplicar_strassen(M4, M4, 32);
+//     auto t1 = std::chrono::system_clock::now();
+//     EXPECT_FLOAT_EQ(M5[0][0], M5.size());
+
+//     auto t2 = std::chrono::system_clock::now();
+//     Matriz M6 = multiplicar_strassen(M4, M4, M4.size());
+//     auto t3 = std::chrono::system_clock::now();
+//     EXPECT_FLOAT_EQ(M5[0][0], M5.size());
+//     auto d = double((t1-t0).count()) / (t3-t2).count();
+//     std::cout << "D: " << d << std::endl;
+//     EXPECT_LT(d, 0.4);
+// }
